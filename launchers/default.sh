fd27d1df12027ef495bf8b217b7e287f0ffc02ce
lineojcd <45730495+lineojcd@users.noreply.github.com>
2020-11-08 22:09:14 +0100

    Add files via upload

---
+++ launchers/default.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
+
+# launching app
+# dt-exec echo "This is an empty launch script. Update it to launch your application."
+
+# roscore &
+# sleep 3
+
+# dt-exec Xvfb :34 -screen 0 1024x768x24 -ac +extension GLX +render -noreset 
+# export DISPLAY=:34
+# dt-exec rosrun my_package my_ros_wrapper.py
+
+roslaunch my_package multiple_nodes.launch veh:=$VEHICLE_NAME
+
+
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# wait for app to end
+dt-launchfile-join
